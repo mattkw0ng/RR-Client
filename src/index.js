a11786c61ea000bfd5871005924eb3b2ff54e080
Matt <makwong@dons.usfca.edu>
2024-09-30 11:52:30 -0700

    axios send cookies

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
+axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
