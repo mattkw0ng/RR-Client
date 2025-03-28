c12e7bc91b6ef939a3c863934abe03d15d15dd7e
Matt <makwong@dons.usfca.edu>
2025-03-27 18:34:41 -0700

    RoomsContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import AdminPage from './components/pages/AdminPage';
 import { AuthProvider } from './context/AuthContext';
+import { RoomsProvider } from './context/RoomsContext';
 
 import RoomRes from './components/pages/RoomRes'; // Assuming you have a home page
 import Login from './components/Login';
@@ -27,25 +28,27 @@ const App = () => {
 
   return (
     <AuthProvider>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/profile"
-            element={<UserProfile />}
-          />
-          <Route path="/search-rooms" element={<RoomSearch auth={auth} />} />
-          <Route path="/room-reservation-form" element={<RoomRes auth={auth} />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path='/admin/*' element={<ProtectedRoute requiredRoles={['admin', 'superadmin']} />} >
-            <Route path='*' element={<AdminPortal />} />
-          </Route>
-          <Route element={<AdminPortal />}>
-            <Route path='/admin-test' element={<AdminPortal />} />
-          </Route>
-        </Routes>
-      </Router>
+      <RoomsProvider>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/profile"
+              element={<UserProfile />}
+            />
+            <Route path="/search-rooms" element={<RoomSearch auth={auth} />} />
+            <Route path="/room-reservation-form" element={<RoomRes auth={auth} />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path='/admin/*' element={<ProtectedRoute requiredRoles={['admin', 'superadmin']} />} >
+              <Route path='*' element={<AdminPortal />} />
+            </Route>
+            <Route element={<AdminPortal />}>
+              <Route path='/admin-test' element={<AdminPortal />} />
+            </Route>
+          </Routes>
+        </Router>
+      </RoomsProvider>
     </AuthProvider>
   );
 };
---
+++ src/context/RoomsContext.js
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import API_URL from "../config";
+import axios from "axios";
+
+const RoomsContext = createContext();
+
+export const useAuth = () => useContext(RoomsContext);
+
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setRooms] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Fetch user data on initial load
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/api/rooms`, { withCredentials: true });;
+        console.log("RoomsContext response", response.data)
+        setRooms(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setUser(false)
+      }
+    };
+
+    fetchRooms();
+  }, []);
+
+  useEffect(() => {
+    if (rooms !== null) {
+      setLoading(false);
+    }
+    console.log("room data updated: ", rooms);
+  }, [rooms]);
+
+  useEffect(() => {
+    console.log("loading (rooms) status updated: ", loading);
+  }, [loading]);
+
+  return (
+    <RoomsContext.Provider value={{ rooms, setRooms, loading }}>
+      {children}
+    </RoomsContext.Provider>
+  );
+};
