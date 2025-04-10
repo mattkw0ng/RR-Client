ec750c88f99e41b8ddcffb5467ccc885149f99c3
Matt <makwong@dons.usfca.edu>
2025-04-10 11:17:47 -0700

    fix

--- src/components/form/AddRoomForm.js
+++ src/components/form/AddRoomForm.js
@@ -27,7 +27,7 @@ export default function AddRoomForm() {
   }
 
   useEffect(() => {
-    console.log("Logging Form Data bc it is not working for some reason:", formData, formData[room_name]);
+    console.log("Logging Form Data bc it is not working for some reason:", formData, formData['room_name']);
   }, [])
 
   return (
